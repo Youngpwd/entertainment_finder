5be3047d4116211cca4617ea7bbb5abfa9f168e3
Charles Young <cbyoung415@gmail.com>
2022-08-31 14:45:55 -0700

    header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <h1 className="header">
+      Entertainment
+      <span className="highlight">
+        <b>Finder</b>
+      </span>
+    </h1>
+  );
+};
+
+export default Header;
