4714103fb8dc00f4e333197c8da844546a9f27c6
Charles Young <cbyoung415@gmail.com>
2022-08-31 14:46:07 -0700

    add routes and render header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import './App.css';
+import { Container } from "@mui/material";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 
+import "./App.css";
 
 function App() {
-
+  return (
+    <div className="app">
+      <Header />
+      <div className="app-container">
+        <Container>
+          <Routes>
+            <Route />
+            <Route />
+            <Route />
+            <Route />
+            <Route path="*" element={<pageNotFound />} />
+          </Routes>
+        </Container>
+      </div>
+      {/* <SimpleBottomNavigaton /> */}
+    </div>
+  );
 }
 
 export default App;
