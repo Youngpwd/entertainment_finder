79164cff14b0e60e7ef564d86d1fb088b55cacbc
Charles Young <cbyoung415@gmail.com>
2022-09-01 11:05:50 -0700

    add pagenotfound default jsx

---
+++ src/routes/PageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <span className="pageTitle">Page Not Found!</span>
+    </div>
+  );
+};
+
+export default PageNotFound;
