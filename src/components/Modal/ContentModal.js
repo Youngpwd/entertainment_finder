bcdd9f70e83031c66e303824510dc096073f8401
Charles Young <cbyoung415@gmail.com>
2022-09-01 11:21:14 -0700

    create modal for each poster

---
+++ src/components/Modal/ContentModal.js
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from "react";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import axios from "axios";
+import {
+  img_500,
+  unavailable,
+  unavailableLandscape,
+} from "../../config/config";
+import "./ContentModal.css";
+import { Button } from "@mui/material";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "90%",
+  height: "80%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "rgb(46, 44, 44);",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  color: "white",
+};
+
+const ContentModal = ({ children, media_type, id }) => {
+  const [open, setOpen] = useState(false);
+  const [content, setContent] = useState();
+  const [video, setVideo] = useState();
+
+  const fetchData = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
+    // console.log(data);
+    setContent(data);
+  };
+
+  const fetchVideo = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
+    // console.log(data);
+    setVideo(data.results[0]?.key);
+  };
+
+  useEffect(() => {
+    fetchData();
+    fetchVideo();
+    // eslint-disable-next-line
+  }, []);
+
+  const handleOpen = () => setOpen(true);
+
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <div onClick={handleOpen} className="media">
+        {children}
+      </div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          {content && (
+            <Box sx={style}>
+              <div className="ContentModal">
+                <img
+                  className="ContentModal__portrait"
+                  src={
+                    content.poster_path
+                      ? `${img_500}/${content.poster_path}`
+                      : unavailable
+                  }
+                  alt={content.name || content.title}
+                />
+                <img
+                  className="ContentModal__landscape"
+                  src={
+                    content.poster_path
+                      ? `${img_500}/${content.backdrop_path}`
+                      : unavailableLandscape
+                  }
+                  alt={content.name || content.title}
+                />
+                <div className="ContentModal__about">
+                  <span className="ContentModal__title">
+                    {content.name || content.title} (
+                    {(
+                      content.first_air_date ||
+                      content.release_date ||
+                      "-----"
+                    ).substring(0, 4)}
+                    )
+                  </span>
+                  {content.tagline && (
+                    <i className="tagline">{content.tagline}</i>
+                  )}
+
+                  <span className="ContentModal__description">
+                    {content.overview}
+                  </span>
+                  <div></div>
+                  <Button
+                    variant="contained"
+                    startIcon={<YouTubeIcon />}
+                    sx={{ backgroundColor: "red", fontWeight: "700" }}
+                    target="_blank"
+                    href={`https://www.youtube.com/watch?v=${video}`}
+                  >
+                    Watch the Trailer
+                  </Button>
+                </div>
+              </div>
+            </Box>
+          )}
+        </Fade>
+      </Modal>
+    </>
+  );
+};
+export default ContentModal;
