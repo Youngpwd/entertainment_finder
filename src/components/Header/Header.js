cf735b83c6496ced110ab76207226afbda8408f6
Charles Young <cbyoung415@gmail.com>
2022-09-01 11:06:03 -0700

    and click event to scroll to top

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <h1 className="header">
+    <h1 className="header" onClick={() => window.scrollTo(0, 0)}>
       Entertainment
       <span className="highlight">
         <b>Finder</b>
